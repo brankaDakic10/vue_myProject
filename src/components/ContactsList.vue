f90a8016b23e13881416bba2975edd4a7a28100d
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-23 21:39:58 +0200

    Added form and table

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -1,22 +1,32 @@
 <template>
   <div >
-    <table>
-  <tr>
+   
+ <form @submit.prevent='addContact'>
+      <label>First name</label>
+      <input v-model='newContact.firstName' type='text' placeholder='First name'>
+      <label>Last name</label>
+      <input v-model='newContact.lastName'type='text' placeholder='Last name'>
+      <label>Email</label>
+      <input v-model='newContact.email' type='email' placeholder='Email'>
+      <button type='submit'>Add contact</button>
+  </form>
+
+
+ <table>
+  <thead>
     <th>Firstname</th>
     <th>Lastname</th> 
     <th>Email</th>
-  </tr>
-  <tr 
-  v-for="(contact, key) in contacts" 
-  :key="key" 
-  v-if="contact.firstName ==='John'">
+  </thead>
+  <tbody>
+  <tr v-for="(contact, key) in contacts" :key="key">
 
     <td>{{contact.firstName}}</td>
     <td>{{contact.lastName}}</td>
    <td>{{contact.email}}</td>
-   
+
   </tr>
- 
+   </tbody>
 </table>
     
   </div>
@@ -32,9 +42,26 @@ export default {
      {firstName:'John',lastName:'Hohn',email:'john@yahoo.com'},
      {firstName:'Ann', lastName:'Jonson',email:'ana@yahoo.com'},
      {firstName:'Sam', lastName:'Smith', email:'sam@yahoo.com'}
-   ]
+   ],
+
+   newContact:{
+       firstName:'',
+       lastName:'',
+       email:''
+         
+      }
     }
     
+  },
+
+  methods:{
+    addContact(){
+       this.contacts.push(this.newContact)
+       this.newContact={}
+
+    }
+
+
   }
 }
 </script>
