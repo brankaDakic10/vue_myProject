920c56227fee6ae939ab6e2b75a3af3bba9fd1c3
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-23 21:01:43 +0200

    Added my_project

---
+++ src/components/ContactsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div >
+    <table>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Email</th>
+  </tr>
+  <tr v-for="(contact, key) in contacts" :key="key">
+    <td>{{contact.firstName}}</td>
+    <td>{{contact.lastName}}</td>
+   <td>{{contact.email}}</td>
+  </tr>
+ 
+</table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactsList',
+  data(){
+    return{
+
+   contacts:[
+     {firstName:'John',lastName:'Hohn',email:'john@yahoo.com'},
+     {firstName:'Ann', lastName:'Jonson',email:'ana@yahoo.com'},
+     {firstName:'Sam', lastName:'Smith', email:'sam@yahoo.com'}
+   ]
+    }
+    
+  }
+}
+</script>
+
+
+</style>
