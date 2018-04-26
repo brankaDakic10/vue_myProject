44dc7cfeb7b99997a06f96a9e8a88e0eee722f9a
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-26 18:56:35 +0200

    Added PropsExample

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <div id="app">
     
-    <ContactsList/>
+   <PropsExample :message=" message"
+     @notify-parent="handleNotifyParent"/>
   </div>
 </template>
 
 <script>
 import ContactsList from './components/ContactsList.vue'
+import PropsExample from './components/PropsExample.vue'
 
 export default {
   name: 'app',
   components: {
-   ContactsList
+   ContactsList,
+   PropsExample
+  },
+
+  data(){
+    return{
+    message:'Hello,world!'
+    }
+  },
+  created(){
+ setTimeout(()=> {
+    this.message ="New message"
+    }, 3000)
+
   }
+  ,
+  methods:{
+    handleNotifyParent(message){
+      this.message=message
+    }
+  }
+  
 }
 </script>
 
---
+++ src/components/PropsExample.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+   {{message}}
+ <button @click='notifyParent'> Notify Parent</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['message'],
+  methods:{
+
+   notifyParent(){
+       this.$emit('notify-parent','Hello from child component')
+   }
+
+
+  }
+}
+</script>
+
+<style>
+</style>
