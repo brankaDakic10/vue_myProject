41c2329ea86715c9d18d5dd1e5e7c32905277d6c
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-26 20:12:19 +0200

    Added router-view

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,16 @@
 <template>
   <div id="app">
-    
-   <PropsExample :message=" message"
-     @notify-parent="handleNotifyParent"/>
-  </div>
+  
+   <router-view></router-view>
+   </div>
 </template>
 
 <script>
-import ContactsList from './components/ContactsList.vue'
-import PropsExample from './components/PropsExample.vue'
 
 export default {
   name: 'app',
-  components: {
-   ContactsList,
-   PropsExample
-  },
-
-  data(){
-    return{
-    message:'Hello,world!'
-    }
-  },
-  created(){
- setTimeout(()=> {
-    this.message ="New message"
-    }, 3000)
-
-  }
-  ,
-  methods:{
-    handleNotifyParent(message){
-      this.message=message
-    }
-  }
-  
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
