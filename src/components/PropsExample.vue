d7f6047b1477c105f7bfc4c0cb93e4adb43a830e
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-26 19:27:06 +0200

    Added watched property on Example

--- src/components/PropsExample.vue
+++ src/components/PropsExample.vue
@@ -2,7 +2,7 @@
   <div >
    {{ message }}
    <article>{{ reversedMessage }}</article>
-   
+   <input type="text" v-model="searchTerm">
  <button @click='notifyParent'> Notify Parent</button>
   </div>
 </template>
@@ -10,6 +10,12 @@
 <script>
 export default {
   props:['message'],
+
+  data(){
+      return{
+          searchTerm:''
+      }
+  },
   methods:{
 
    notifyParent(){
@@ -21,6 +27,14 @@ computed:{
     reversedMessage(){
      return this.message.split('').reverse().join('')
     }
+ }
+ , 
+ watch:{
+   searchTerm(newValue, oldValue){
+       console.log('New Value: ',newValue)
+        console.log('Old Value: ',oldValue)
+   }
+
  }
 }
 </script>
